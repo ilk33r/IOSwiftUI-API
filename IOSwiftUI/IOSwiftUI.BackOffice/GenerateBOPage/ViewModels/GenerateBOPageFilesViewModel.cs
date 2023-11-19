c52c3bf4c965d32d10bcd0ae655b3c89bc3b40a1
Ilker OZCAN <iletisim@ilkerozcan.com.tr>
2023-11-19 12:59:42 +0300

    Framework was updated.

---
+++ IOSwiftUI/IOSwiftUI.BackOffice/GenerateBOPage/ViewModels/GenerateBOPageFilesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using IOBootstrap.NET.BackOffice;
+using IOSwiftUI.DataAccess.Context;
+
+namespace IOSwiftUI.BackOffice;
+
+public class GenerateBOPageFilesViewModel : IOGenerateBOPageFilesViewModel<DatabaseContext>
+{
+    public GenerateBOPageFilesViewModel() : base()
+    {
+    }
+}
